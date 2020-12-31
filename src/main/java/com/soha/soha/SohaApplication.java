ad66a0bf791b34f9730fd7976305d4f72c1753d7
pirooz <55871049+pirooz193@users.noreply.github.com>
2020-12-31 12:44:52 +0330

    Update SohaApplication.java

--- src/main/java/com/soha/soha/SohaApplication.java
+++ src/main/java/com/soha/soha/SohaApplication.java
@@ -1,12 +1,9 @@
 package com.soha.soha;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class SohaApplication {
