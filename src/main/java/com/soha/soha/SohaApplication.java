3f6a93c5da34879898ddbe5239c47e914abb6c24
pirooz <55871049+pirooz193@users.noreply.github.com>
2020-12-31 12:45:53 +0330

    initial commit

--- src/main/java/com/soha/soha/SohaApplication.java
+++ src/main/java/com/soha/soha/SohaApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SohaApplication {
-
-
+    
     public static void main(String[] args) {
         SpringApplication.run(SohaApplication.class, args);
     }
