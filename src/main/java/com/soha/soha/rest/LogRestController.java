3ff71d46942577218a00f253eb52d8237c038e48
pirooz <piroozazarabad@gmail.com>
2021-01-01 21:09:37 +0330

    Set Logger to Log Responses And Requests .

--- src/main/java/com/soha/soha/rest/LogRestController.java
+++ src/main/java/com/soha/soha/rest/LogRestController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @RestController
 @RequestMapping("/api")
@@ -16,14 +18,17 @@ public class LogRestController {
 
 
     @GetMapping("/log")
-    public String logRest() {
+    public String logRest(HttpServletRequest request) {
         try {
 
-            RestTemplate restTemplate = new RestTemplate();
-            String resourceUrl = "https://random-data-api.com/api/users/random_user";
-            ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl, String.class);
 
-            AppLogger.logResponsesAndRequests(response, LogRestController.class);
+            RestTemplate restTemplate = new RestTemplate();
+            String fooResourceUrl = "https://random-data-api.com/api/users/random_user";
+            ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+
+            AppLogger.logRequest(request, LogRestController.class);
+            AppLogger.logResponses(response, LogRestController.class);
+
             return response.getBody();
 
         } catch (Exception e) {
@@ -32,4 +37,5 @@ public class LogRestController {
         }
 
     }
+
 }
--- src/main/java/com/soha/soha/soap/LogSoapController.java
+++ src/main/java/com/soha/soha/soap/LogSoapController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @RestController
 @RequestMapping("/soap")
@@ -14,20 +16,21 @@ public class LogSoapController {
 
 
     @GetMapping("/log")
-    public String logSoap() {
+    public String logSoap(HttpServletRequest request) {
         try {
 
             RestTemplate restTemplate = new RestTemplate();
             String resourceUrl = "https://www.crcind.com/csp/samples/SOAP.Demo.cls?wsdl";
             ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl, String.class);
 
-            AppLogger.logResponsesAndRequests(response, LogSoapController.class);
+            AppLogger.logRequest(request, LogSoapController.class);
+            AppLogger.logResponses(response, LogSoapController.class);
 
             return response.getBody();
-        } catch (Exception e) {
 
+        } catch (Exception e) {
             e.printStackTrace();
-            return "Oops !! ";
+            return "OOPS !! ";
         }
 
     }
