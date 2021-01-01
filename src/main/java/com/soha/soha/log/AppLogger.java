a6b90c1573d7de033704f01eb79793b0cbd13bb0
pirooz <piroozazarabad@gmail.com>
2021-01-01 21:09:16 +0330

    Set Logger to Log Responses And Requests .

--- src/main/java/com/soha/soha/log/AppLogger.java
+++ src/main/java/com/soha/soha/log/AppLogger.java
@@ -1,16 +1,28 @@
 package com.soha.soha.log;
 
-
+import com.soha.soha.rest.LogRestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class AppLogger {
+
+    /**
+     * This Method id Logging  Requests
+     *
+     * @param request
+     * @param clazz
+     */
+    public static void logRequest(HttpServletRequest request, Class clazz) {
+        Logger logger = LoggerFactory.getLogger(clazz);
+        logger.info("Request Method : " + request.getMethod());
+        logger.info("Request Address : " + request.getRemoteAddr());
+        logger.info("Request URL : " + request.getRequestURL().toString());
+        logger.info("Request URI : " + request.getRequestURI());
+    }
 
     /**
      * this method logs the response and requests( in the file and console )
@@ -19,24 +31,12 @@ public class AppLogger {
      * @param clazz
      * @throws IOException
      */
-    public static void logResponsesAndRequests(ResponseEntity<String> response, Class clazz) throws IOException {
-
-        //LOG  IN THE FILE
-        File file = new File("C:\\Users\\Kamtel\\Downloads\\demo\\soha\\src\\main\\resources\\logs.txt");
-        file.createNewFile();
-        FileWriter fileWriter = new FileWriter(file, true);
-        BufferedWriter writer = new BufferedWriter(fileWriter);
-        writer.append(" Header : ");
-        writer.append(response.getHeaders().toString());
-        writer.append(" / ");
-        writer.append("Body : ");
-        writer.append(response.getBody());
-        writer.close();
-
-        //LOG IN THE CONSOLE
+    public static void logResponses(ResponseEntity<String> response, Class clazz) {
         Logger logger = LoggerFactory.getLogger(clazz);
-        logger.info(response.getHeaders().toString());
-        logger.info(response.getBody());
+        logger.info("Header : " + response.getHeaders().toString());
+        logger.info("Body : " + response.getBody());
 
     }
+
+
 }
